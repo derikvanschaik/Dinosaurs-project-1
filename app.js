869e44fa53a325f87f4964560980682c01a6247a
derikvanschaik <derikvanschaik@gmail.com>
2021-05-02 13:27:46 -0700

    image can now be displayed onto tile grid

--- app.js
+++ app.js
@@ -1,9 +1,91 @@
+function getDinoObjects(){
+    dino = {"Dinos": [
+        {
+            "species": "Triceratops",
+            "weight": 13000,
+            "height": 114,
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "First discovered in 1889 by Othniel Charles Marsh"
+        },
+        {
+            "species": "Tyrannosaurus Rex",
+            "weight": 11905,
+            "height": 144,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "The largest known skull measures in at 5 feet long."
+        },
+        {
+            "species": "Anklyosaurus",
+            "weight": 10500,
+            "height": 55,
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Anklyosaurus survived for approximately 135 million years."
+        },
+        {
+            "species": "Brachiosaurus",
+            "weight": 70000,
+            "height": "372",
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Jurasic",
+            "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+        },
+        {
+            "species": "Stegosaurus",
+            "weight": 11600,
+            "height": 79,
+            "diet": "herbavor",
+            "where": "North America, Europe, Asia",
+            "when": "Late Jurasic to Early Cretaceous",
+            "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+        },
+        {
+            "species": "Elasmosaurus",
+            "weight": 16000,
+            "height": 59,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+        },
+        {
+            "species": "Pteranodon",
+            "weight": 44,
+            "height": 20,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+        },
+        {
+            "species": "Pigeon",
+            "weight": 0.5,
+            "height": 9,
+            "diet": "herbavor",
+            "where": "World Wide",
+            "when": "Holocene",
+            "fact": "All birds are living dinosaurs."
+        }
+    ]
+    }
+    return dino['Dinos']; 
+}
+
+const dinos = getDinoObjects();
+
 class Human{
-    constructor(name, feet, inches, weight, diet){
+    constructor(name, feet, inches, weight, diet, image){
         this.name = name;
         this.height = Number(feet) + Number(inches)/12;
         this.weight = Number(weight);
         this.diet = diet;
+        this.image = image; 
     }
     introduce(){
         return `hello I am ${this.name} and i am a ${this.diet}`; 
@@ -21,27 +103,30 @@ function getHuman(){
     const feet = getValue('feet');
     const inches = getValue('inches');
     const weight = getValue('weight');
-    const diet = getValue('diet'); 
-    return new Human(name, feet, inches, weight, diet); 
+    const diet = getValue('diet');
+    const image = 'human.png'; 
+    return new Human(name, feet, inches, weight, diet, image);
 }
 
 function hideForm(){
     document.getElementById('dino-compare').style.display = 'none';
 }
 
-function makeTile(text){
-    const grid = document.getElementById('grid'); 
+function makeTile(object){
+    const grid = document.getElementById('grid');
     let newTile = document.createElement('div');
-    let tileText = document.createTextNode(text);
-    newTile.append(tileText);
-    grid.append(newTile); 
-
+    newTile.classList.add('grid-item'); 
+    newTile.innerHTML = `
+    <h3>${object.name}</h3>
+    <img src="images/${object.image}">
+    `;
+    grid.appendChild(newTile); 
 }
 
 function loadNewPage(vals, ids){
     const human = getHuman();
     hideForm(); 
-    makeTile(human.introduce());
+    makeTile(human);
 }
 
 const validField = (field) => {return field.length > 0}; 
